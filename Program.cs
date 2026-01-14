5b131aff182a22040c76d57a89c2a52b58055c80
miron-kwiatkowski <miron.kwiatkowski@studenci.collegiumwitelona.pl>
2026-01-14 13:34:53 +0100

    fixed swagger, fixed open api

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using MiniStrava;
 using MiniStrava.Repositories;
 using MiniStrava.Services;
@@ -20,7 +21,41 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // =======================
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "MiniStrava API",
+        Version = "v1"
+    });
+
+    // JWT w Swagger UI (Authorize button)
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "Wpisz: Bearer {twój_token}"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
+
 
 // =======================
 // DI - repozytoria/serwisy auth (Twoje istniej¹ce)
@@ -76,9 +111,20 @@ app.UseApiExceptions();
 
 // =======================
 // Swagger
+// UI: /api/documentation
+// JSON: /api/documentation/v1/swagger.json
 // =======================
-app.UseSwagger();
-app.UseSwaggerUI();
+app.UseSwagger(c =>
+{
+    c.RouteTemplate = "api/documentation/{documentName}/swagger.json";
+});
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/api/documentation/v1/swagger.json", "MiniStrava API v1");
+    c.RoutePrefix = "api/documentation";
+});
+
 
 // =======================
 // Auth
--- obj/Debug/net8.0/MiniStrava.AssemblyInfo.cs
+++ obj/Debug/net8.0/MiniStrava.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiniStrava")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+11b076db727d26cbc8742b493a530b0a32a76cb7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+635cc9c31f6daabeb4d967a31305855557214a3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiniStrava")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiniStrava")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
